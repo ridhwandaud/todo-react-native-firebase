fb910200aa3dac68162f855762a07d949800ac49
Ridhwan <muhammadridhwandaud@gmail.com>
2018-05-27 08:19:53 +0800

    Remove TodoCreate screen
    - not use anymore , instead use modal

--- src/containers/Router.js
+++ src/containers/Router.js
@@ -3,14 +3,12 @@ import { View, Text, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import StartScreen from './Start';
 import HomeScreen from './HomeScreen';
-import TodoCreate from './HomeScreen/TodoCreate';
 import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
 const AuthStack = createStackNavigator({ StartScreen: StartScreen });
 const AppStack = createStackNavigator(
 	{ 
-		Home: HomeScreen, 
-		TodoCreate: TodoCreate 
+		Home: HomeScreen
 	},
 	{
 		navigationOptions: {
