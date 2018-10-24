a50f8cddc770b13d564027ecc5db07b43ad58566
Ridhwan <muhammadridhwandaud@gmail.com>
2018-10-24 11:14:54 +0800

    Fixed firebase

--- android/app/src/main/java/com/ridhwandaud/todoit/MainApplication.java
+++ android/app/src/main/java/com/ridhwandaud/todoit/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
+import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,7 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
           new RNFirebasePackage(),
           new VectorIconsPackage(),
           new RNFirebaseAuthPackage(),
-          new RNFirebaseDatabasePackage()
+          new RNFirebaseDatabasePackage(),
+          new RNFirebaseRemoteConfigPackage()
       );
     }
 
